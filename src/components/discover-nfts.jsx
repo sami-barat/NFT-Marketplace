aefc78e6ac1e4d3633468e3b5a86c8167d8efa2e
sami-barat <sami.xbarat.12@gmail.com>
2024-03-26 09:47:39 +0500

    few changes

--- src/components/discover-nfts.jsx
+++ src/components/discover-nfts.jsx
@@ -7,9 +7,8 @@ import Avatar1 from "../assets/images/discovernfts/Avatar1.png";
 import Avatar2 from "../assets/images/discovernfts/Avatar2.png";
 import Avatar3 from "../assets/images/discovernfts/Avatar3.png";
 import "./styles/discovernfts.css";
-import { Button } from './button';
+import { Button } from "./button";
 import { LuEye } from "react-icons/lu";
-
 
 const DiscoverNfts = () => {
   const discoverData = [
@@ -44,29 +43,23 @@ const DiscoverNfts = () => {
 
   return (
     <>
-      <div style={{backgroundColor: "#2B2B2B", paddingTop: '100px'}}>
+      <div style={{ backgroundColor: "#2B2B2B", paddingTop: "100px" }}>
         <div className="discover_section container">
-          <div style={{
-            display:'flex',
-            flexDirection: 'row',
-            justifyContent:'space-between',
-            alignItems: 'center',
-            paddingBottom: '60px'
-          }}>
+          <div className="discover-content">
             <div>
-              <p style={{fontWeight: '600', fontSize: '38px', paddingBottom: '15px'}}>Discover more NFT's</p>
-              <p style={{fontWeight: '400', fontSize: '22px'}}>Explore New Trending NFT's</p>
+              <p className="heading">Discover more NFT's</p>
+              <p className="sub-heading">Explore New Trending NFT's</p>
             </div>
             <Button
               w={187}
               h={60}
-              color= "#FFFFFF"
+              color="#FFFFFF"
               bcolor="transparent"
               text="See All"
               ff={"'Work Sans', sans-serif"}
               alignItems
               fw={600}
-              b= "2px solid #A259FF"
+              b="2px solid #A259FF"
               fz="16px"
               brs={20}
               icon={<LuEye size={20} color="#A259FF" />}
@@ -76,21 +69,27 @@ const DiscoverNfts = () => {
             {discoverData.map((item) => (
               <div key={item.id} className="informations">
                 <img src={item.Image} alt={item.name} />
-                <div style={{margin: '15px 25px'}}>
-                  <p style={{fontWeight: '600', fontSize: '22px', paddingBottom: '10px'}}>{item.name}</p>
-                  <div style={{display: 'flex'}}>
-                    <img src={item.avatarImage} alt={item.avatarName} className="avatar_image"/>
-                    <p style={{fontWeight: '400', fontSize: '16px', fontFamily: "'Space Mono', monospace", marginLeft: '10px'}}>{item.avatarName}</p>
+                <div style={{ margin: "15px 25px" }}>
+                  <p className="text-1">{item.name}</p>
+                  <div style={{ display: "flex" }}>
+                    <img
+                      src={item.avatarImage}
+                      alt={item.avatarName}
+                      className="avatar_image"
+                    />
+                    <p className="text-2">{item.avatarName}</p>
                   </div>
                 </div>
-                <div style={{ display: "flex", justifyContent: 'space-between', margin: '25px 25px'}}>
-                  <div style={{fontFamily: "'Space Mono', monospace", fontWeight: '400'}}>
-                    <p style={{color: '#858584', fontSize: '12px',  }}>Price</p>
-                    <p style={{fontSize: '16px'}}>{item.Price}</p>
+                <div className="information-2">
+                  <div>
+                    <p style={{ color: "#858584", fontSize: "12px" }}>Price</p>
+                    <p style={{ fontSize: "16px" }}>{item.Price}</p>
                   </div>
-                  <div style={{fontFamily: "'Space Mono', monospace", fontWeight: '400'}}>
-                    <p style={{color: '#858584', fontSize: '12px',  }}>Highest Bid</p>
-                    <p style={{fontSize: '16px'}}>{item.highestBid}</p>
+                  <div>
+                    <p style={{ color: "#858584", fontSize: "12px" }}>
+                      Highest Bid
+                    </p>
+                    <p style={{ fontSize: "16px" }}>{item.highestBid}</p>
                   </div>
                 </div>
               </div>
